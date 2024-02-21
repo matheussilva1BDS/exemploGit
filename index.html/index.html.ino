bab0f089094d6f6ac942d9905bff8dbf938cb229
Math3usSilva <matheus.silvafutbol13@gmail.com>
2024-02-21 10:10:19 -0300

    initial commit

---
+++ index.html/index.html.ino
@@ -0,0 +1,14 @@
+//declaracao das variaveis / numeros reais//
+float nota1, nota2, nota3, peso1, peso2, peso3, media;
+
+void setup() {
+//inicia a comunicacao com o Serial monitor
+ Serial.begin(9600);
+delay(1000);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
